8464ac9e2172c93b16ca3c4da1d2237c40bc6d79
hh.joo <joohhcse@gmail.com>
2020-04-03 17:39:51 +0900

    JSP_03

---
+++ JSP_03/src/com/jsp/servlet/LoginServlet.java
@@ -0,0 +1,61 @@
+package com.jsp.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doGet()");
+
+		request.getRequestDispatcher("/WEB-INF/views/common/loginForm.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("doPost()");
+		
+		String url = "/WEB-INF/views/common/login_success.jsp";
+		
+		String id = request.getParameter("id");
+		String pwd = request.getParameter("pwd");
+		
+		//로그인 처리		//MemberVO member = mService.getMember(id);
+//		response.sendRedirect("/main");
+		
+		if(!(id.equals("mimi") && pwd.equals("mimi"))) {
+			request.setAttribute("id", id);
+			url = "/WEB-INF/views/common/loginForm.jsp";
+		}
+		else {
+			//로그인성공
+			MemberVO member = new MemberVO();
+			member.setId(id);
+			member.setPwd(pwd);
+			member.setEmail("mimi");
+			
+			HttpSession session = request.getSession();
+			session.setAttribute("loginUser", member);
+			
+			System.out.println(member.toString());
+
+		}
+		
+		request.getRequestDispatcher(url).forward(request,  response);
+		
+		System.out.println(url);
+	}
+
+}
